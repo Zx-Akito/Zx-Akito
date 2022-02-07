99e893c38d20f6d2c7dc7bebceca1a0cac635a34
Zx-Akito <82266736+Zx-Akito@users.noreply.github.com>
2022-02-07 09:11:15 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    error_reporting(0);
-
-    session_start();
-    
-    // include ("app/db.php"); // Koneksi Database
-    include ("app/function.php"); // Fungsi
-    include ("app/routing.php"); // Penghubung Halaman
-?>
