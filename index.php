91846236d297a1459a2c88f8ae31fb77800142d8
Zx-Akito <82266736+Zx-Akito@users.noreply.github.com>
2022-02-07 09:08:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    error_reporting(0);
+
+    session_start();
+    
+    // include ("app/db.php"); // Koneksi Database
+    include ("app/function.php"); // Fungsi
+    include ("app/routing.php"); // Penghubung Halaman
+?>
